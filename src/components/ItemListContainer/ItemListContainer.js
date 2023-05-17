8531354afdce88bc6ba9a7e433d5652e0d789f5d
Maria Sol Martin <mariasol.m92@hotmail.com>
2023-05-17 11:43:16 -0300

    segunda entrega

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,29 +1,30 @@
 import { useEffect, useState } from "react"
-import { getProducts } from '../asyncMock'
-import ItemListContainer from '../ItemListContainer/ItemListContainer'
+import { getProducts, getProductsByCategory } from '../asyncMock'
+import { useParams } from "react-router-dom";
 
-function ItemListContainer({ greeting }) {
+
+const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([false]);
-    const { categoryId } = useParams();
-    useEffect(() => {
+    const { categoryId } = useParams ()
+    useEffect( () => {
         const asyncFunc = categoryId ? getProductsByCategory : getProducts;
 
         asyncFunc(categoryId)
             .then(response => {
-                setProducts(response);
+                setProducts(response)
             })
             .catch(error => {
-                console.error(error);
-            });
-    }, [categoryId]);
+                console.error(error)
+            })
+    }, [categoryId])
     return (
         <div>
             <h1>{greeting}</h1>
             <div>
-                <ItemList products={products} />
+                <ItemList products = {products} />
             </div>
         </div>
-    );
+    )
 
 }
 export default ItemListContainer
